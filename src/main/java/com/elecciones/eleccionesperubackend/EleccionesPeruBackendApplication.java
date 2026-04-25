c7fdc063b61ef02f85aed774b12fd17d39af7e57
jcastili <jul_ed@hotmail.com>
2026-04-24 19:46:27 -0500

    feat: initial commit with business logic and readme

---
+++ src/main/java/com/elecciones/eleccionesperubackend/EleccionesPeruBackendApplication.java
@@ -0,0 +1,13 @@
+package com.elecciones.eleccionesperubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EleccionesPeruBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EleccionesPeruBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elecciones/eleccionesperubackend/EleccionesPeruBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elecciones.eleccionesperubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EleccionesPeruBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
